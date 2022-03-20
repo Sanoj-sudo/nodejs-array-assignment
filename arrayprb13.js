35c99a4ec17558267f95d54f23ee9cdf05efc6de
Sanoj Kumar <sanoj.kumar@programmers.io>
2022-03-20 21:46:11 +0530

    print the count above threshold value of array

---
+++ arrayprb13.js
@@ -0,0 +1,10 @@
+const getAboveThresholdValue = (parameter1,parameter2) =>{
+    let count =0;
+    for (let index=0; index <parameter1.length; index++){
+        if (parameter1[index] > parameter2){
+            count++;
+        } 
+    }
+    return count;
+}
+console.log(getAboveThresholdValue([1,2,3,4,5,3,6],3));
